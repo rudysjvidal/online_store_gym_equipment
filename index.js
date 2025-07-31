6704aae32e3052de02e7bd3625c2647b3ffda213
Alice Smith <alice@webage.com>
2025-07-31 11:46:28 -0400

    THIS IS A SECRET IM ADDING

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 // run `node index.js` in the terminal
 
 console.log(`Hello Node.js v${process.versions.node}!`);
+
+// secret-test.js
+const MY_SECRET_KEY = "12345abcdef67890";
+console.log("This is a secret:", MY_SECRET_KEY);
